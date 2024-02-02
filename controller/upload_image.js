29195169ef4eca78dcbc45f6e848a77eec1733ea
Rajkiranb55 <141542918+Rajkiranb55@users.noreply.github.com>
2024-02-03 02:12:11 +0530

    updated

--- controller/upload_image.js
+++ controller/upload_image.js
@@ -4,7 +4,7 @@ const port = process.env.API_URL;
 const uploadImage = async (req, res) => {
   res.json({
     success: 1,
-    image_url: `${port}/${req.file.filename}`,
+    image_url: `https://skribble-frontend.vercel.app/${req.file.filename}`,
   });
 };
 
