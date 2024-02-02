5749508b29e3e5e2b3c36ccbf85b3f33415d9c2a
Rajkiranb55 <141542918+Rajkiranb55@users.noreply.github.com>
2024-02-02 09:54:29 +0530

    updated a mistake in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
-const cors = require("cors");
+// const cors = require("cors");
 const dotenv = require("dotenv").config();
 
 ///////////////////////
